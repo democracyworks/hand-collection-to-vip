d4e061f738650525cd004eb80ceb1be53628200c
Victoria Levchenko <vglevchenko@gmail.com>
2018-09-06 11:46:24 -0400

    Script to check causes for Google reported errors

--- 2018/vip_earlyvoting_google_errors.py
+++ 2018/vip_earlyvoting_google_errors.py
@@ -11,9 +11,9 @@ import os
 from zipfile import ZipFile
 from IPython.display import display
 import warnings
+from prettypandas import PrettyPandas
 warnings.filterwarnings('ignore')
 pd.set_option('max_colwidth', 200)
-
 
 
 # PRO-TIP: if modifying these scopes, delete the file token.json.
@@ -101,17 +101,75 @@ if __name__ == '__main__':
                 state_feed = state_feed_all[state_feed_all['state_abbrv'] == state] # FILTER state_feed_all for selected state
                 election_authorities = election_authorities_all[election_authorities_all['state'] == state] # FILTER election_authorities_all for selected state
                 
+                # CREATE dataframes of unique OCD IDs for election authorities and state data
+                ea = election_authorities[['ocd_division']]
+                ea.drop_duplicates(inplace=True)
+                sd = state_data[['OCD_ID']]
+                sd.drop_duplicates(inplace=True)
+
+                title = 'Investigating Errors Reported by Google' # SET title
+                length = (32*2)+len(title) # LENGTH of width
+                
+                # MERGE & PRINT lists
                 print()
                 print()
-                print('<<<<<ERROR CHECKING>>>>>')
+                print(title.center(length, ' '))
+                print()
+                print('(1) OCD IDs in hand collection data but not election authorities'.center(length, ' '))
+                print('(2) OCD IDs in election authorities but not hand collection data'.center(length, ' '))
+                print('(3) Empty values in hand collection data'.center(length, ' '))
+                print()
+                print('-'*length)
                 print()
                 print()
-                print('OCD ID differences between election authorities and state data')
-                diff = pd.concat([election_authorities['ocd_division'],state_data['OCD_ID']]).drop_duplicates(keep=False)
-                print(diff)
+                print('(1)'.center(length, ' '))
                 print()
-                print('Count of empty cells per column in state data')
+                print('OCD IDs unique to hand collection data. They do not match any ids listed in election authorities.'.center(length, ' '))
+                print()
+                diff_state_data = sd.merge(ea, how = 'left', left_on = 'OCD_ID', right_on = 'ocd_division')
+                diff_state_data = diff_state_data[diff_state_data['ocd_division'].isnull()]
+                print(diff_state_data['OCD_ID'])
+                print()
+                print('WHAT THIS INDICATES:'.center(length, ' '), '\n')
+                print('If there are OCD IDs listed here,'.center(length, ' '), '\n', 'they are likely mispelled or county/place is incorrect.'.center(length, ' '))
+                print()
+                print()
+                print('-'*length)
+                print()
+                print()
+                print('(2)'.center(length, ' '))
+                print()
+                print('OCD IDs unqiue to election authorities. They do not match any ids listed in hand collection data.'.center(length, ' '))
+                print()
+                diff_election_authorities = ea.merge(sd, how = 'left', left_on = 'ocd_division', right_on = 'OCD_ID')
+                diff_election_authorities = diff_election_authorities[diff_election_authorities['OCD_ID'].isnull()]
+                print(diff_election_authorities['ocd_division'])
+                print()
+                print('WHAT THIS INDICATES:'.center(length, ' '), '\n')
+                print('If there are OCD IDs listed here,'.center(length, ' '), '\n', 'the county/place is likely missing entirely from hand collection data.'.center(length, ' '))
+                print()
+                print()
+                print('-'*length)
+                print()
+                print()
+                print('(3)'.center(length, ' '))
+                print()
+                print('Count of empty cells per column in hand collection data'.center(length, ' '))
+                print()
                 print(state_data.isnull().sum())
+                print()
+                print('If \'is_only_by_appointment\' includes \'TRUE\', then some empty cells are acceptable for \'is_only_by_appointment\'.')
+                print(state_data['is_only_by_appointment'].value_counts())
+                print()
+                print('If \'is_or_by_appointment\' includes \'TRUE\', then some empty cells are acceptable for \'is_or_by_appointment\'.')
+                print(state_data['is_or_by_appointment'].value_counts())
+                print()
+                print('If \'is_drop_box\' includes \'TRUE\', then some empty cells are acceptable for \'is_drop_box\'.')
+                print(state_data['is_drop_box'].value_counts())
+                print()
+                print('WHAT THIS INDICATES:'.center(length, ' '), '\n')
+                print('If there are empty cells,'.center(length, ' '), '\n', 'outreach may still be collecting data, data was mistakenly omitted,'.center(length, ' '),
+                      '\n', 'or data is not properly loading and/or processing in the associated script.'.center(length, ' '))
                 print()
                 
                 
@@ -128,3 +186,5 @@ if __name__ == '__main__':
             except:
                 print ('ERROR:', state, 'could not be processed.')
                 increment_processingerror += 1
+
+
